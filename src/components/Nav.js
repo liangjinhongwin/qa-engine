c30753522e9719ac07de1c8f9e4fac784643080c
Jinhong Liang <liangjinhongwin@hotmail.com>
2020-03-17 20:31:13 -0700

    .

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,23 +14,23 @@ const Nav = (props) => {
       <nav className="navbar sticky-top navbar-expand-md navbar-dark bg-dark">
         <Link to="/" className="navbar-brand">Q&A Engine</Link>
         {props.auth.auth &&
-          <div>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item" data-toggle="collapse" data-target="#navbarNav">
-                  <span className="nav-link text-white">Hello, {sessionStorage.getItem("auth_user")}!</span>
-                </li>
-                <li className="nav-item" data-toggle="collapse" data-target="#navbarNav">
-                  <Link to="/profile" className="nav-link"><button className="btn btn-info btn-sm">Profile</button></Link>
-                </li>
-                <li className="nav-item" data-toggle="collapse" data-target="#navbarNav">
-                  <Link to="/login" className="nav-link"><button className="btn btn-warning btn-sm" onClick={() => { logout() }}>Log out</button></Link>
-                </li>
-              </ul>
-            </div>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        }
+        {props.auth.auth &&
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item" data-toggle="collapse" data-target="#navbarNav">
+                <span className="nav-link text-white">Hello, {sessionStorage.getItem("auth_user")}!</span>
+              </li>
+              <li className="nav-item" data-toggle="collapse" data-target="#navbarNav">
+                <Link to="/profile" className="nav-link"><button className="btn btn-info btn-sm">Profile</button></Link>
+              </li>
+              <li className="nav-item" data-toggle="collapse" data-target="#navbarNav">
+                <Link to="/login" className="nav-link"><button className="btn btn-warning btn-sm" onClick={() => { logout() }}>Log out</button></Link>
+              </li>
+            </ul>
           </div>
         }
       </nav>
